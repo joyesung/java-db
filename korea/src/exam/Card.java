759948cd81f938d0e3b40ea7a7c408f209d60f39
joyesung <frog0039@naver.com>
2019-05-23 16:49:35 +0900

    5월23일

---
+++ korea/src/exam/Card.java
@@ -0,0 +1,46 @@
+package exam;
+
+public class Card{
+	private int num;
+	private String shape;
+	public int getNum() {
+		return num;
+	}
+	public String getShape() {
+		return shape;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+	@Override
+	public String toString() {
+		String s = new String();
+		if(shape.equals("S")){
+			s += "¢¼";
+		}else if(shape.equals("H")){
+			s += "¢¾";
+		}else if(shape.equals("C")){
+			s += "¢À";
+		}else if(shape.equals("D")){
+			s += "¡ß";
+		}
+
+		if(num == 11)	s+="J";
+		else if(num == 12)	s+="Q";
+		else if(num == 13)	s+="K";
+		else
+			s+=num;
+		return s;
+	}
+	public Card(){}
+	public Card(Card c){
+		this.num = c.num;
+		this.shape = c.shape;
+		
+		
+	
+	}
+}
